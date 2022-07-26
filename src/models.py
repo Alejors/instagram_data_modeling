a82c2b79a874386a133cab90b051bf085dad903b
Alejandro Atria <alejoatria@gmail.com>
2022-07-26 17:00:01 +0000

    agregada tabla files por la opcion de multiples imagenes en un post

--- src/models.py
+++ src/models.py
@@ -17,24 +17,30 @@ class User(Base):
 class Follower(Base):
     __tablename__ = 'followers'
     user_from_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('users.id'))
 
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    photo = Column(String(250), nullable=False)
     text = Column(String(250))
     likes = Column(Integer)
 
+class File(Base):
+    __tablename__ = 'files'
+    id = Column(Integer, primary_key=True)
+    filetype = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
 class Liked(Base):
     __tablename__ = 'liked_posts'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
 
 class Comment(Base):
     __tablename__ = 'comments'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
     text = Column(String(250))
     
